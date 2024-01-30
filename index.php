ad4c8ffd08efbd1414bcc10e73d8c21bbe4bb8cd
ademinho <ademben7877@gmail.com>
2024-01-30 14:33:04 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include 'partials/header.php' ?>
+
+<div class="wrapper">
+    <H1>Bienvenue sur mon app php</H1>
+    <img src="./assets/font/image/kisspng-ronaldo-football-player-real-madrid-c-f-uefa-euro-5afabde84abbf6.3261439015263820563061.jpg" alt="">
+</div>
+
+    
+<?php include 'partials/footer.php' ?>
---
+++ partials/header.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="./views/style/style.css">
+<link rel="stylesheet" href="../views/style/style.css">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eshop PHP</title>
+</head>
+<body>
+
+<nav>
+     <ul>
+         <li><a href="../index.php">Accueil</a></li>
+         <li><a href="./views/products.view.php">Produits</a></li>
+         <li><a href="contact.view.php">Contact</a></li>
+         <li><a href="signup.view.php">Signup</a></li>
+     </ul>
+
+</nav>
